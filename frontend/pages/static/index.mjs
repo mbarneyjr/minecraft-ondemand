f0f58961cdf7465f87ef184a4c984747d6853864
Michael Barney, Jr <mbarneyme@gmail.com>
2023-11-20 23:54:32 -0500

    fix(lint): disable bucket policy cfn-lint warning

--- frontend/pages/static/index.mjs
+++ frontend/pages/static/index.mjs
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+// @ts-nocheck
+// eslint-disable-next-line import/no-unresolved
 import * as components from '@mbarneyjr/webcomponents';
 
 components.utils.theme.init();

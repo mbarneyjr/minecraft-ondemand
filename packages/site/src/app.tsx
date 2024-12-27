1ad1571c39369779c76db8c5c56fe94b01f162c0
Michael Barney, Jr <mbarneyme@gmail.com>
2024-12-27 11:45:28 -0500

    fix(site): cleanup app handler

--- packages/site/src/app.tsx
+++ packages/site/src/app.tsx
@@ -1,6 +1,5 @@
-import { fileURLToPath } from 'url';
 import { Hono } from 'hono';
-import type { FC } from 'hono/jsx';
+import { FC } from 'hono/jsx';
 import { handle } from 'hono/aws-lambda';
 import { Favicon } from '#src/icons/favicon.js';
 import { HomePage } from '#src/pages/home-page.js';
@@ -21,27 +20,3 @@ app.get('/favicon.ico', async (c) => {
 app.route('/whitelist', whitelist);
 
 export const handler = handle(app);
-
-if (process.argv[1] === fileURLToPath(import.meta.url)) {
-  const fs = await import('fs');
-  const { join } = await import('path');
-  const url = await import('url');
-  const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
-  app.use(
-    (await import('hono/serve-static')).serveStatic({
-      root: join(__dirname, 'public'),
-      rewriteRequestPath: (path) => path.replace(/^\/public/, ''),
-      getContent: async (path) => {
-        console.log('GET static assets', path);
-        try {
-          return fs.readFileSync(path, 'utf-8');
-        } catch (e) {
-          return null;
-        }
-      },
-    }),
-  );
-  (await import('@hono/node-server')).serve(app, (info) => {
-    console.log(`Listening on http://localhost:${info.port}`);
-  });
-}
